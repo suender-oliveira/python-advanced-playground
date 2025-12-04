73c0a427e6ff3c360698d79eb3d4b7040514242b
Suender Oliveira <suender@live.com>
2025-12-04 19:28:52 -0300

    Initial Commit

---
+++ mypylib/__about__.py
@@ -0,0 +1,3 @@
+__title__ = "mypylib"
+__version__ = "0.1.0"
+__author__ = "Suender Batista de Oliveira"
---
+++ mypylib/__init__.py
@@ -0,0 +1,7 @@
+from .pure import *
+from .decorators import *
+from .contextmanagers import *
+from .async_utils import *
+from .types_ import *
+
+__all__ = [*list(globals().keys())]
